0dc6bfa46f758fb913745865a151214a778ed93f
thanho <thanho.dut@gmail.com>
2023-06-06 22:18:42 +0700

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include "Arduino_FreeRTOS.h"
 
+#define LED1_PIN  2
+#define LED2_PIN  3
+
+
+
 // put function declarations here:
 void Task1(void* pvParameters);
 void Task2(void* pvParameters);
@@ -35,3 +40,33 @@ void setup()
 
 
 void loop() {}
+
+/*.....................Functions.............................
+.............................................................
+.............................................................
+*/
+
+/// @brief Taks 1, Led blinking
+/// @param pvParameters 
+void Task1(void *pvParameters __attribute__((unused)))
+{
+  // Inittialze
+  pinMode(LED1_PIN, OUTPUT);
+  digitalWrite(LED1_PIN, false);
+
+  //...Loop
+  while(true)
+  {
+    digitalWrite(LED1_PIN, false);
+    vTaskDelay(100);
+    digitalWrite(LED1_PIN, true);
+    vTaskDelay(100);
+  }
+}
+
+/// @brief Task 2, Serial debug
+/// @param pvParameters 
+void Task2(void *pvParameters __attribute__((unused)))
+{
+
+}
