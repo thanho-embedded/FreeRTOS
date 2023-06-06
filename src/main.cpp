f9bff32aab77ab7750bbdda76317f2233cd85080
thanho <thanho.dut@gmail.com>
2023-06-06 22:12:14 +0700

    Create freeRTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,37 @@
 #include <Arduino.h>
+#include "Arduino_FreeRTOS.h"
 
 // put function declarations here:
+void Task1(void* pvParameters);
+void Task2(void* pvParameters);
 
 
 void setup() 
 {
   // put your setup code here, to run once:
-  
+
+  //...Create a task 1
+  xTaskCreate
+  (
+    Task1,
+    "LED Blinking", // Name of task
+    128,            // Stack size 128 bytes
+    NULL,           // Parameters for task (pvParameters)
+    2,              // Priority, 0 is lowest
+    NULL            // Task handle
+  );
+  //...Create a task 2
+  xTaskCreate
+  (
+    Task2,
+    "Debug Monitor",  // Name of task
+    128,              // Stack size 128 bytes
+    NULL,             // Parameters for task (pvParameters)
+    2,                // Priority, 0 is lowest
+    NULL              // Task handle
+  );
 }
 
-void loop() 
-{
-  // put your main code here, to run repeatedly:
-}
+
+
+void loop() {}
